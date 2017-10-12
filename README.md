f404ecdfe618c364d529e0874885afb2645e0ee8
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-10-12 22:36:34 +0300

    add LICENSE, CHANGELOG, init users and web services, add Mongo and PostgreSQL images

--- README.md
+++ README.md
@@ -5,9 +5,19 @@ Overview
 
 Cinema is an example project which demonstrates the use of microservices for a fictional movie theater.
 
-The backend is powered by 3 microservices, all of witch happen to be written in
+The backend is powered by 4 microservices, all of witch happen to be written in
 Go.
 
  * Channels Service: Provides information about existing channels.
  * Messages Service: Provides communicating information.
  * Users Service: Provides messages for users by communicating with other services.
+ * Web Service: Represents web interface for customer.
+
+Commands
+========
+Starting services: `docker-compose up --build` or
+using script `./bin/start.sh`
+
+Stopping services: `docker-compose stop`
+
+Deploying lates changes: `docker-compose build`
---
+++ users/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+)
+
+// User - user account model
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// userType - GraphQL ObjectType which represents user account
+var userType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "User",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"name": &graphql.Field{
+			Type: graphql.String,
+		},
+		"email": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
+
+// rootMutation - root mutation
+var rootMutation = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootMutation",
+	Fields: graphql.Fields{
+		"registerAccount": &graphql.Field{
+			Type:        userType,
+			Description: "register a new account",
+			Args: graphql.FieldConfigArgument{
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"email": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				name, _ := params.Args["name"].(string)
+				email, _ := params.Args["email"].(string)
+
+				newAccount := User{
+					Name:  name,
+					Email: email,
+				}
+
+				return newAccount, nil
+			},
+		},
+	},
+})
+
+var rootQuery = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootQuery",
+	Fields: graphql.Fields{
+		"user": &graphql.Field{
+			Type:        userType,
+			Description: "Get single user details",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				_, isOk := params.Args["id"].(string)
+				if isOk {
+					// todo: find and return user info
+				}
+
+				return User{}, nil
+			},
+		},
+		"usersList": &graphql.Field{
+			Type:        graphql.NewList(userType),
+			Description: "Get list of users",
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return nil, nil
+			},
+		},
+	},
+})
+
+// define schema, with our rootQuery and rootMutation
+var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query:    rootQuery,
+	Mutation: rootMutation,
+})
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
+	}
+	return result
+}
+
+// Entry point for the program
+func main() {
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := executeQuery(r.URL.Query().Get("query"), schema)
+		json.NewEncoder(w).Encode(result)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
